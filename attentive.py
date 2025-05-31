4e12251b2e0fe02dceb72237f7660d95e3bd1e41
hlm01 <35647316+hlm01@users.noreply.github.com>
2025-05-31 15:50:39 -0700

    a

--- attentive.py
+++ attentive.py
@@ -8,13 +8,28 @@ from rdkit import Chem
 from torch_geometric.datasets import MoleculeNet
 from torch_geometric.loader import DataLoader
 from attentive_fp import AttentiveFP
+from tqdm import tqdm
 
 
 class GenFeatures:
     def __init__(self):
         self.symbols = [
-            'B', 'C', 'N', 'O', 'F', 'Si', 'P', 'S', 'Cl', 'As', 'Se', 'Br',
-            'Te', 'I', 'At', 'other'
+            "B",
+            "C",
+            "N",
+            "O",
+            "F",
+            "Si",
+            "P",
+            "S",
+            "Cl",
+            "As",
+            "Se",
+            "Br",
+            "Te",
+            "I",
+            "At",
+            "other",
         ]
 
         self.hybridizations = [
@@ -23,7 +38,7 @@ class GenFeatures:
             Chem.rdchem.HybridizationType.SP3,
             Chem.rdchem.HybridizationType.SP3D,
             Chem.rdchem.HybridizationType.SP3D2,
-            'other',
+            "other",
         ]
 
         self.stereos = [
@@ -39,27 +54,33 @@ class GenFeatures:
 
         xs = []
         for atom in mol.GetAtoms():
-            symbol = [0.] * len(self.symbols)
-            symbol[self.symbols.index(atom.GetSymbol())] = 1.
-            degree = [0.] * 6
-            degree[atom.GetDegree()] = 1.
+            symbol = [0.0] * len(self.symbols)
+            symbol[self.symbols.index(atom.GetSymbol())] = 1.0
+            degree = [0.0] * 6
+            degree[atom.GetDegree()] = 1.0
             formal_charge = atom.GetFormalCharge()
             radical_electrons = atom.GetNumRadicalElectrons()
-            hybridization = [0.] * len(self.hybridizations)
-            hybridization[self.hybridizations.index(
-                atom.GetHybridization())] = 1.
-            aromaticity = 1. if atom.GetIsAromatic() else 0.
-            hydrogens = [0.] * 5
-            hydrogens[atom.GetTotalNumHs()] = 1.
-            chirality = 1. if atom.HasProp('_ChiralityPossible') else 0.
-            chirality_type = [0.] * 2
-            if atom.HasProp('_CIPCode'):
-                chirality_type[['R', 'S'].index(atom.GetProp('_CIPCode'))] = 1.
+            hybridization = [0.0] * len(self.hybridizations)
+            hybridization[self.hybridizations.index(atom.GetHybridization())] = 1.0
+            aromaticity = 1.0 if atom.GetIsAromatic() else 0.0
+            hydrogens = [0.0] * 5
+            hydrogens[atom.GetTotalNumHs()] = 1.0
+            chirality = 1.0 if atom.HasProp("_ChiralityPossible") else 0.0
+            chirality_type = [0.0] * 2
+            if atom.HasProp("_CIPCode"):
+                chirality_type[["R", "S"].index(atom.GetProp("_CIPCode"))] = 1.0
 
-            x = torch.tensor(symbol + degree + [formal_charge] +
-                             [radical_electrons] + hybridization +
-                             [aromaticity] + hydrogens + [chirality] +
-                             chirality_type)
+            x = torch.tensor(
+                symbol
+                + degree
+                + [formal_charge]
+                + [radical_electrons]
+                + hybridization
+                + [aromaticity]
+                + hydrogens
+                + [chirality]
+                + chirality_type
+            )
             xs.append(x)
 
         data.x = torch.stack(xs, dim=0)
@@ -71,17 +92,18 @@ class GenFeatures:
             edge_indices += [[bond.GetEndAtomIdx(), bond.GetBeginAtomIdx()]]
 
             bond_type = bond.GetBondType()
-            single = 1. if bond_type == Chem.rdchem.BondType.SINGLE else 0.
-            double = 1. if bond_type == Chem.rdchem.BondType.DOUBLE else 0.
-            triple = 1. if bond_type == Chem.rdchem.BondType.TRIPLE else 0.
-            aromatic = 1. if bond_type == Chem.rdchem.BondType.AROMATIC else 0.
-            conjugation = 1. if bond.GetIsConjugated() else 0.
-            ring = 1. if bond.IsInRing() else 0.
-            stereo = [0.] * 4
-            stereo[self.stereos.index(bond.GetStereo())] = 1.
+            single = 1.0 if bond_type == Chem.rdchem.BondType.SINGLE else 0.0
+            double = 1.0 if bond_type == Chem.rdchem.BondType.DOUBLE else 0.0
+            triple = 1.0 if bond_type == Chem.rdchem.BondType.TRIPLE else 0.0
+            aromatic = 1.0 if bond_type == Chem.rdchem.BondType.AROMATIC else 0.0
+            conjugation = 1.0 if bond.GetIsConjugated() else 0.0
+            ring = 1.0 if bond.IsInRing() else 0.0
+            stereo = [0.0] * 4
+            stereo[self.stereos.index(bond.GetStereo())] = 1.0
 
             edge_attr = torch.tensor(
-                [single, double, triple, aromatic, conjugation, ring] + stereo)
+                [single, double, triple, aromatic, conjugation, ring] + stereo
+            )
 
             edge_attrs += [edge_attr, edge_attr]
 
@@ -95,25 +117,30 @@ class GenFeatures:
         return data
 
 
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'AFP_Mol')
-dataset = MoleculeNet(path, name='ESOL', pre_transform=GenFeatures()).shuffle()
+path = osp.join(osp.dirname(osp.realpath(__file__)), "data", "AFP_Mol")
+dataset = MoleculeNet(path, name="ESOL", pre_transform=GenFeatures()).shuffle()
 
 N = len(dataset) // 10
 val_dataset = dataset[:N]
-test_dataset = dataset[N:2 * N]
-train_dataset = dataset[2 * N:]
+test_dataset = dataset[N : 2 * N]
+train_dataset = dataset[2 * N :]
 
-train_loader = DataLoader(train_dataset, batch_size=200, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=200, shuffle=True, pin_memory=True)
 val_loader = DataLoader(val_dataset, batch_size=200)
 test_loader = DataLoader(test_dataset, batch_size=200)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = AttentiveFP(in_channels=39, hidden_channels=200, out_channels=1,
-                    edge_dim=10, num_layers=2, num_timesteps=2,
-                    dropout=0.2).to(device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = AttentiveFP(
+    in_channels=39,
+    hidden_channels=200,
+    out_channels=1,
+    edge_dim=10,
+    num_layers=2,
+    num_timesteps=2,
+    dropout=0.2,
+).to(device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=10**-2.5,
-                             weight_decay=10**-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=10**-2.5, weight_decay=10**-5)
 
 
 def train():
@@ -136,13 +163,15 @@ def test(loader):
     for data in loader:
         data = data.to(device)
         out = model(data.x, data.edge_index, data.edge_attr, data.batch)
-        mse.append(F.mse_loss(out, data.y, reduction='none').cpu())
+        mse.append(F.mse_loss(out, data.y, reduction="none").cpu())
     return float(torch.cat(mse, dim=0).mean().sqrt())
 
 
-for epoch in range(1, 500):
+for epoch in tqdm(range(1, 500)):
     train_rmse = train()
     val_rmse = test(val_loader)
     test_rmse = test(test_loader)
-    print(f'Epoch: {epoch:03d}, Loss: {train_rmse:.4f} Val: {val_rmse:.4f} '
-          f'Test: {test_rmse:.4f}')
+    # print(
+    #     f"Epoch: {epoch:03d}, Loss: {train_rmse:.4f} Val: {val_rmse:.4f} "
+    #     f"Test: {test_rmse:.4f}"
+    # )
